234f3fe091b70d33ff37725cb65fbb2484e0d063
SangWoo Park <psu8430@gmail.com>
2025-10-29 20:48:50 +0900

    feat: TodoPage 컴포넌트 추가

---
+++ src/components/TodoPage.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useTodos } from '@/composables/useTodos';
+import TodoHeader from './TodoHeader.vue';
+import TodoAddButton from './TodoAddButton.vue';
+import TodoItem from './TodoItem.vue';
+
+const { todos, loading, addTodo } = useTodos();
+</script>
+
+<template>
+  <div class="todo-page-container">
+    <TodoHeader />
+    <TodoAddButton class="todo-add-button" @onSubmit="addTodo" />
+    <div v-if="loading">Loading...</div>
+    <div v-else class="todo-list">
+      <TodoItem v-for="todo in todos" :key="todo.id" v-bind="todo" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.todo-page-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.todo-add-button {
+  margin-bottom: 16px;
+}
+.todo-list {
+  flex: 1;
+  overflow: auto;
+}
+</style>
