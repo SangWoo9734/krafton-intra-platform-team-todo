bf8c41991d01cde7149a83da57bbe04b2f85acc5
SangWoo Park <psu8430@gmail.com>
2025-10-29 13:07:43 +0900

    feat: BasicChip 컴포넌트 추가

---
+++ src/components/BasicChip.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import type { ImportantLabelVarients } from '@/types/todo';
+
+interface Props {
+  text: string;
+  variant?: ImportantLabelVarients;
+  class?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  variant: 'medium',
+  class: '',
+});
+
+const chipVariantStyle: Record<ImportantLabelVarients, string> = {
+  high: 'bg-red',
+  medium: 'bg-gray',
+  low: 'bg-blue',
+};
+</script>
+
+<template>
+  <div :class="[chipVariantStyle[props.variant], 'chip', props.class]">
+    {{ props.text.toUpperCase() }}
+  </div>
+</template>
+<style scoped>
+.chip {
+  border-radius: 10000px;
+  color: white;
+  font-size: 10px;
+  padding: 3px 8px;
+  width: fit-content;
+  font-weight: bold;
+}
+
+.bg-blue {
+  background-color: #4e83da;
+}
+
+.bg-red {
+  background-color: #ff5656;
+}
+
+.bg-gray {
+  background-color: #8a8a8a;
+}
+</style>
