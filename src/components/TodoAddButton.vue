d74e285f478dc49c42fffe3d453a044b7ebb7baa
SangWoo Park <psu8430@gmail.com>
2025-10-29 20:25:52 +0900

    feat: TodoAddButton 컴포넌트 추가

---
+++ src/components/TodoAddButton.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { PlusCircleIcon } from '@heroicons/vue/20/solid';
+import { useModal } from '@/composables/useModal';
+
+const props = withDefaults(
+  defineProps<{
+    class?: string;
+  }>(),
+  {
+    class: '',
+  },
+);
+
+const { openModal } = useModal();
+
+const handleAddTodo = () => {
+  openModal();
+};
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="handleAddTodo"
+    :class="['todo-add-button', props.class]"
+  >
+    <p>Add a new TODO</p>
+    <PlusCircleIcon class="icon" />
+  </button>
+</template>
+<style scoped lang="scss">
+.todo-add-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+  width: 100%;
+  background-color: #4e83da;
+  padding: 8px 0;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  color: #ffffff;
+  font-weight: bold;
+
+  &:hover {
+    background-color: #4e84dacb;
+  }
+
+  .icon {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+}
+</style>
